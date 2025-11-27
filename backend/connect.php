8d075bc85edb276f8cd323290b75ef72cf078fe4
raykenzienazaru-dot <raykenzienazaru@gmail.com>
2025-11-28 00:24:35 +0700

    update connect.php

--- backend/connect.php
+++ backend/connect.php
@@ -1,5 +1,5 @@
 <?php
     
     $connect = mysqli_connect("localhost","root","","bank_sampah") or die ('database tidak terhubung');
-
+$connect = mysqli_connect("localhost","root","","basada") or die ('database tidak terhubung');
 ?>
